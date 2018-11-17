fe3f32daa241fa2be9d4104cc83fd8820c2b17f9
shotaro-ooe <shoooe97@gmail.com>
2018-11-17 16:45:35 +0900

    ガード節を代用してみた

--- fizzbazz.py
+++ fizzbazz.py
@@ -1,12 +1,12 @@
 def fizzbuzz_convert(number):
     if number % 15 == 0:
        return "fizzbuzz"
-    elif number % 5 == 0:
+    if number % 5 == 0:
        return "Buzz"
-    elif number % 3 == 0:
+    if number % 3 == 0:
        return "Fizz"
-    else:
-        return str(number)
+
+    return str(number)
 
 for number in range(1,101):
         fizzbuzz_convert(number)
